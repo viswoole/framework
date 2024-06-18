6cc7a901c69d85b7e83b6ad46d6c45d959ddcbce
ZhuChongLin <8210856@qq.com>
2024-06-18 15:34:01 +0800

    App应用管理中心

---
+++ src/Core/App.php
@@ -0,0 +1,163 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Core;
+
+use Viswoole\Core\Service\Provider;
+
+/**
+ * App应用管理中心
+ *
+ * @property Env $env 环境变量管理实例
+ * @property Config $config 配置管理实例
+ */
+class App extends Container
+{
+  protected static self $instance;
+  /**
+   * @var array<string,string> 接口标识映射
+   */
+  protected array $bindings = [
+    'app' => App::class,
+    'env' => Env::class,
+    'config' => Config::class,
+  ];
+  /**
+   * @var Provider[] 服务列表
+   */
+  protected array $services = [];
+
+  protected function __construct()
+  {
+    self::$instance = $this;
+    $this->bind(App::class, $this);
+    $this->initialize();
+  }
+
+  private function initialize(): void
+  {
+    // 系统默认时区
+    date_default_timezone_set(
+      $this->config->get(
+        'app.default_timezone',
+        'Asia/Shanghai'
+      )
+    );
+    // 注册服务
+    $this->registerService();
+    // 启动服务
+    $this->bootService();
+  }
+
+  /**
+   * 注册服务
+   *
+   * @access protected
+   * @return void
+   */
+  protected function registerService(): void
+  {
+    $services = $this->config->get('app.services', []);
+    $depPath = $this->getVendorPath() . '/services.php';
+    // 依赖包注册的服务
+    $dependentServices = is_file($depPath) ? require $depPath : [];
+    $services = array_merge($services, $dependentServices);
+    // 遍历服务绑定进容器
+    foreach ($services as $service) {
+      if (is_string($service)) $service = new $service($this);
+      if (property_exists($service, 'bindings')) {
+        $this->bindings = array_merge($this->bindings, $service->bindings);
+      }
+      $service->register();
+      $this->services[] = $service;
+    }
+  }
+
+  /**
+   * 获取vendor路径
+   *
+   * @return string
+   */
+  public function getVendorPath(): string
+  {
+    return $this->getRootPath() . DIRECTORY_SEPARATOR . 'vendor';
+  }
+
+  /**
+   * 获取项目根路径
+   *
+   * @access public
+   * @return string
+   */
+  public function getRootPath(): string
+  {
+    !defined('BASE_PATH') && define('BASE_PATH', dirname(realpath(__DIR__), 4));
+    return rtrim(BASE_PATH, DIRECTORY_SEPARATOR);
+  }
+
+  /**
+   * 初始化服务
+   *
+   * @return void
+   */
+  protected function bootService(): void
+  {
+    foreach ($this->services as $service) $service->boot();
+  }
+
+  /**
+   * 工厂单例模式
+   *
+   * @return App
+   */
+  public static function factory(): App
+  {
+    if (!isset(self::$instance)) new static();
+    return self::$instance;
+  }
+
+  /**
+   * 获取config路径
+   *
+   * @access public
+   * @return string
+   */
+  public function getConfigPath(): string
+  {
+    return $this->getRootPath() . DIRECTORY_SEPARATOR . 'config';
+  }
+
+  /**
+   * 获取app路径
+   *
+   * @access public
+   * @return string
+   */
+  public function getAppPath(): string
+  {
+    return $this->getRootPath() . DIRECTORY_SEPARATOR . 'app';
+  }
+
+  /**
+   * 获取env路径
+   *
+   * @access public
+   * @return string
+   */
+  public function getEnvPath(): string
+  {
+    return $this->getRootPath() . DIRECTORY_SEPARATOR . '.env';
+  }
+}
---
+++ tests/AppTest.php
@@ -0,0 +1,125 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Viswoole\Core\App;
+use Viswoole\Core\Config;
+
+class AppTest extends TestCase
+{
+  protected App $app;
+
+  public function testGet()
+  {
+    /** @var $config Config */
+    $config = $this->app->get('config');
+    self::assertInstanceOf(Config::class, $config);
+  }
+
+  public function testInvokeFunction()
+  {
+    $fn = function (App $app, mixed $test) {
+      return $test;
+    };
+    $result = $this->app->invokeFunction($fn, [$this->app, 'test']);
+    self::assertEquals('test', $result);
+  }
+
+  public function testMake()
+  {
+    $config = $this->app->make('config');
+    self::assertInstanceOf(Config::class, $config);
+  }
+
+  public function testBind()
+  {
+    $this->app->bind('test', function (App $app) {
+      return $app;
+    });
+    $test = $this->app->make('test');
+    self::assertInstanceOf(App::class, $test);
+  }
+
+  public function testRemoveHook()
+  {
+    $callback = function (MyTest $test, App $app) {
+      throw new \Exception('收到了MyTest类被反射解析的回调');
+    };
+    $callback2 = function (MyTest $test, App $app) {
+      echo '收到了MyTest类被反射解析的回调2' . PHP_EOL;
+    };
+    $this->app->addHook(MyTest::class, $callback);
+    $this->app->addHook(MyTest::class, $callback2);
+    $this->app->removeHook(MyTest::class, $callback);
+    $this->testInvokeClass();
+  }
+
+  public function testInvokeClass()
+  {
+    $instance = $this->app->invokeClass(MyTest::class);
+    self::assertInstanceOf(MyTest::class, $instance);
+  }
+
+  public function testInvokeMethod()
+  {
+    $result = $this->app->invokeMethod([MyTest::class, 'test']);
+    self::assertEquals('test', $result);
+  }
+
+  public function testAddHook()
+  {
+    $this->app->addHook(MyTest::class, function (MyTest $test, App $app) {
+      echo '收到了MyTest类被反射解析的回调' . PHP_EOL;
+      self::assertTrue(true);
+    });
+    $this->testInvokeClass();
+  }
+
+  public function testHas()
+  {
+    $result = $this->app->has('config');
+    self::assertTrue($result);
+  }
+
+  public function testRemove()
+  {
+    $this->app->bind('test', MyTest::class);
+    $this->app->make('test');
+    $result = $this->app->has(MyTest::class);
+    self::assertTrue($result);
+    $this->app->remove('test');
+    $result = $this->app->has(MyTest::class);
+    self::assertFalse($result);
+  }
+
+  protected function setUp(): void
+  {
+    $this->app = App::factory();
+  }
+}
+
+class MyTest
+{
+  public function __construct(public App $app)
+  {
+  }
+
+  public function test()
+  {
+    return 'test';
+  }
+}
