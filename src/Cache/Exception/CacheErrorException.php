41ad2f4e25821d206a9516f1842a92f3d98f46e8
ZhuChongLin <8210856@qq.com>
2024-07-04 12:27:15 +0800

    缓存异常类

---
+++ src/Cache/Exception/CacheErrorException.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Cache\Exception;
+
+use RuntimeException;
+
+class CacheErrorException extends RuntimeException
+{
+
+}
