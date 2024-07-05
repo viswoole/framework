553125ace2f3243ebf61b02d250d52d1b9936c35
ZhuChongLin <8210856@qq.com>
2024-07-05 13:49:46 +0800

    数据验证异常类

---
+++ src/Core/Exception/ValidateException.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Core\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * 验证异常
+ */
+class ValidateException extends InvalidArgumentException
+{
+  /**
+   * 验证错误信息
+   * @var string|array
+   */
+  protected string|array $error;
+
+  public function __construct(string|array $error)
+  {
+    $this->error = $error;
+    $message = is_array($error) ? implode(PHP_EOL, $error) : $error;
+    parent::__construct(message: $message);
+  }
+
+  /**
+   * 获取验证错误信息
+   * @access public
+   * @return array|string
+   */
+  public function getError(): array|string
+  {
+    return $this->error;
+  }
+}
