648930e8fd51b0b87169380f64bf0c8d8fffbff9
ZhuChongLin <8210856@qq.com>
2024-07-18 17:47:25 +0800

    新增验证数值min

---
+++ src/Core/Validate/Rules/Min.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Core\Validate\Rules;
+
+use Attribute;
+
+/**
+ * 验证数值小于或等于$min
+ */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
+class Min extends RuleAbstract
+{
+  /**
+   * @param int $min 最小值
+   * @param string $message
+   */
+  public function __construct(
+    public int $min,
+    string     $message = ''
+  )
+  {
+    parent::__construct($message);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  #[\Override] public function validate(mixed $value): int|float
+  {
+    if (!is_numeric($value)) $this->error('必须为数值类型');
+    $value = is_float($this->min) ? floatval($value) : intval($value);
+    if ($value < $this->min) {
+      $this->error("必须大于或等于 $this->min");
+    }
+    return $value;
+  }
+}
