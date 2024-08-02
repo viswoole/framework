9911d300ae43c0d011a6148e34b77eeb132c8d01
ZhuChongLin <8210856@qq.com>
2024-08-02 11:45:48 +0800

    更新缓存配置

--- config/cache.php
+++ config/cache.php
@@ -7,7 +7,6 @@ declare (strict_types=1);
 
 use Viswoole\Cache\Driver\File;
 use Viswoole\Cache\Driver\Redis;
-use Viswoole\Cache\RedisConfig;
 
 return [
   // 默认通道
@@ -16,11 +15,8 @@ return [
   'stores' => [
     // 驱动类需继承Viswoole\Cache\Driver，
     // 或实现Viswoole\Cache\Contract\CacheDriverInterface接口。
-    // 通道支持给定驱动类完全限定名称或数组形式配置，Options接受传递给驱动类的构造参数。
+    // 通道支持给定驱动类完全限定名称、驱动实例、数组形式配置['diver'=>diver::class,'options'=>object|array]，options接受传递给驱动类的构造参数。
     'file' => File::class,
-    'redis' => [
-      'driver' => Redis::class,
-      'options' => new RedisConfig(env('REDIS_HOST', '127.0.0.1'))
-    ],
+    'redis' => new Redis(env('REDIS_HOST', '127.0.0.1'))
   ]
 ];
