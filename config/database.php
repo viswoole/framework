9c8380f2675d7790abde827cbd5d85a55f1e533f
ZhuChongLin <8210856@qq.com>
2024-09-01 14:32:41 +0800

    调整配置属性

--- config/database.php
+++ config/database.php
@@ -18,14 +18,15 @@
 declare (strict_types=1);
 
 use Viswoole\Database\Channel\PDO\PDOChannel;
+use Viswoole\Database\Db;
 
 return [
   // 默认通道
   'default' => env('DATABASE_DEFAULT', 'default'),
   // 是否开启调试模式
   'debug' => env('app_debug', true),
-  // 调试信息保存方式logger|console 输出到日志或输出到控制台
-  'debug_info_save' => 'console',
+  // 调试信息保存方式，1保存到控制台，2保存到日志文件，3保存到控制台和日志文件
+  'info_save_manner' => Db::DEBUG_SAVE_CONSOLE | Db::DEBUG_SAVE_LOGGER,
   // 通道列表
   'channels' => [
     // 驱动类需继承Viswoole\Database\Channel
