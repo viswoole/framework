7b03fb18c13fe00f6941f08489d0ddb1bb64e3a0
ZhuChongLin <8210856@qq.com>
2024-09-01 15:35:59 +0800

    优化echo方法

--- src/Core/Console/Output.php
+++ src/Core/Console/Output.php
@@ -15,6 +15,8 @@ declare (strict_types=1);
 
 namespace Viswoole\Core\Console;
 
+use Exception;
+
 /**
  * 输出类，使用该类可轻松打印各种颜色的消息到控制台。
  *
@@ -22,6 +24,9 @@ namespace Viswoole\Core\Console;
  * @method static void warning(string $message, int $backtrace = 1) 黄色
  * @method static void info(string $message, int $backtrace = 1) 默认颜色
  * @method static void error(string $message, int $backtrace = 1) 红色
+ * @method static void emergency(string $message, int $backtrace = 1) 红色
+ * @method static void alert(string $message, int $backtrace = 1) 红色
+ * @method static void critical(string $message, int $backtrace = 1) 红色
  * @method static void notice(string $message, int $backtrace = 1) 蓝色
  * @method static void debug(string $message, int $backtrace = 1) 灰色
  */
@@ -42,15 +47,18 @@ class Output
     'WHITE' => "\033[1;37m",   // 白色（加粗）
   ];
   /**
-   * 等级颜色映射
+   * 标签颜色映射
    */
-  public const array LEVEL_COLOR = [
+  public const array LABEL_COLOR = [
+    'EMERGENCY' => self::COLORS['RED'],
+    'ALERT' => self::COLORS['RED'],
+    'CRITICAL' => self::COLORS['RED'],
+    'DEBUG' => self::COLORS['GREY'],
     'ERROR' => self::COLORS['RED'],
     'WARNING' => self::COLORS['YELLOW'],
     'INFO' => self::COLORS['DEFAULT'],
     'SUCCESS' => self::COLORS['GREEN'],
     'NOTICE' => self::COLORS['BLUE'],
-    'DEBUG' => self::COLORS['GREY'],
   ];
 
   /**
@@ -59,7 +67,7 @@ class Output
    * @access public
    * @param mixed $data 变量内容
    * @param string $title 标题
-   * @param string $color 颜色
+   * @param string $color 颜色,可以传入标签名称，如：'SUCCESS'
    * @param int $backtrace 1为输出调用源，0为不输出
    * @return void
    */
@@ -70,8 +78,8 @@ class Output
     int    $backtrace = 1
   ): void
   {
-    if (array_key_exists($color, self::LEVEL_COLOR)) {
-      $color = self::LEVEL_COLOR[$color];
+    if (array_key_exists($color, self::LABEL_COLOR)) {
+      $color = self::LABEL_COLOR[$color];
     } else {
       $console_color_pattern = '/^(\033)\[[0-9;]+m$/';
       $isColor = preg_match($console_color_pattern, $color);
@@ -112,11 +120,19 @@ class Output
     return 'in ' . $filename . ':' . $line;
   }
 
+  /**
+   * @param string $name
+   * @param array $arguments
+   * @return void
+   * @throws Exception
+   */
   public static function __callStatic(string $name, array $arguments)
   {
     $name = strtoupper($name);
-    if (array_key_exists($name, self::LEVEL_COLOR)) {
-      self::echo($arguments[0], $name, $arguments[1] ?? 1);
+    if (array_key_exists($name, self::LABEL_COLOR)) {
+      self::echo($arguments[0] ?? '', $name, backtrace: $arguments[1] ?? 1);
+    } else {
+      throw new Exception("Call to undefined method $name");
     }
   }
 
@@ -124,13 +140,15 @@ class Output
    * 输出一条文本信息
    *
    * @param string|int $message 要输出的内容
-   * @param string $color 颜色，支持传入等级或转义颜色
+   * @param string $label 标签
+   * @param string|null $color 转义颜色,如果标签未映射颜色，且传入null，则使用默认颜色
    * @param int $backtrace 1为输出调用源，0为不输出
    * @return void
    */
   public static function echo(
     string|int $message,
-    string     $color = self::COLORS['DEFAULT'],
+    string     $label = 'INFO',
+    ?string    $color = null,
     int        $backtrace = 1
   ): void
   {
@@ -140,13 +158,16 @@ class Output
       $trace = '';
     }
     $date = date('c');
-    if (array_key_exists($color, self::LEVEL_COLOR)) {
-      $color = self::LEVEL_COLOR[$color];
-    } else {
+    $label = strtoupper($label);
+    if (array_key_exists($label, self::LABEL_COLOR) && !$color) {
+      $color = self::LABEL_COLOR[$label];
+    } elseif ($color) {
       $console_color_pattern = '/^(\033)\[[0-9;]+m$/';
       $isColor = preg_match($console_color_pattern, $color);
       $color = $isColor ? $color : self::COLORS['DEFAULT'];
+    } else {
+      $color = self::COLORS['DEFAULT'];
     }
-    echo "{$color}[$date]: $message$trace" . PHP_EOL . self::COLORS['DEFAULT'];
+    echo "{$color}[$date][$label]: $message$trace" . PHP_EOL . self::COLORS['DEFAULT'];
   }
 }
