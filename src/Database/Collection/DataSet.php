f482ce3a34e5d7bbd9ebce4a1b2b06da91b56e54
ZhuChongLin <8210856@qq.com>
2024-09-01 21:54:30 +0800

    增加save方法

--- src/Database/Collection/DataSet.php
+++ src/Database/Collection/DataSet.php
@@ -31,17 +31,6 @@ class DataSet extends BaseCollection
   protected int $flags = ArrayObject::STD_PROP_LIST | ArrayObject::ARRAY_AS_PROPS;
 
   /**
-   * 获取某一列的值
-   *
-   * @param string $column
-   * @return mixed
-   */
-  public function value(string $column): mixed
-  {
-    return $this->offsetGet($column);
-  }
-
-  /**
    * 删除集合中的所有记录
    *
    * @param bool $real 是否为硬删除，默认为false，仅模型查询结果支持$real参数。
@@ -50,7 +39,7 @@ class DataSet extends BaseCollection
    */
   #[Override] public function delete(bool $real = false): int
   {
-    $pk = $this->query->getOptions()->pk;
+    $pk = $this->query->getPrimaryKey();
     if (isset($this[$pk])) {
       return $this->query->where($pk, $this[$pk])->delete($real);
     } else {
@@ -61,7 +50,7 @@ class DataSet extends BaseCollection
   }
 
   /**
-   * 合并数据，相同的字段将被新数据覆盖
+   * 合并数据，相同的字段将被新数据覆盖，如果需要保存数据则接着调用一次save方法即可
    *
    * @param array $data
    * @return static
@@ -72,5 +61,39 @@ class DataSet extends BaseCollection
       $this[$key] = $value;
     }
     return $this;
+  }
+
+  /**
+   * 保存数据
+   *
+   * 会在同一事务中自动更新关联数据
+   *
+   * @return int 保存成功返回1，失败返回0，数据没有任何修改也会返回0
+   */
+  public function save(): int
+  {
+    $pk = $this->query->getPrimaryKey();
+    if (isset($this[$pk])) {
+      $change = [];
+      foreach ($this as $key => $value) {
+        if (!$value instanceof BaseCollection) {
+          $change[$key] = $value;
+        }
+      }
+      return $this->query->update($change);
+    } else {
+      throw new RuntimeException("保存数据失败，缺少主键字段($pk)");
+    }
+  }
+
+  /**
+   * 获取某一列的值
+   *
+   * @param string $column
+   * @return mixed
+   */
+  public function value(string $column): mixed
+  {
+    return $this->offsetGet($column);
   }
 }
