0ad51ffdc69e5084881fedc8d80e2c5e4b8664b8
ZhuChongLin <8210856@qq.com>
2024-09-09 22:22:55 +0800

    优化idea helper 注释

--- src/Cache/CacheManager.php
+++ src/Cache/CacheManager.php
@@ -15,7 +15,10 @@ declare (strict_types=1);
 
 namespace Viswoole\Cache;
 
+use Closure;
+use DateTime;
 use Viswoole\Cache\Contract\CacheDriverInterface;
+use Viswoole\Cache\Contract\CacheTagInterface;
 use Viswoole\Cache\Driver\File;
 use Viswoole\Cache\Driver\Redis;
 use Viswoole\Cache\Exception\CacheErrorException;
@@ -23,6 +26,29 @@ use Viswoole\Core\Config;
 
 /**
  * 缓存驱动管理器
+ *
+ * @method int|false inc(string $key, int $step = 1) 自增缓存（针对数值缓存）
+ * @method mixed get(string $key, mixed $default = null) 获取缓存
+ * @method bool unlock(string $id) 解锁
+ * @method bool set(string $key, mixed $value, DateTime|int|null $expire = null, bool $NX = false) 设置缓存
+ * @method int|false ttl(string $key) 获取缓存剩余有效期 -1为长期有效 false为不存在或过期
+ * @method int|false dec(string $key, int $step = 1) 自减缓存
+ * @method mixed pull(string $key) 获取缓存并删除
+ * @method int|false delete(array|string $keys) 删除缓存
+ * @method bool has(string $key) 判断缓存是否存在
+ * @method bool clear() 清除所有缓存
+ * @method string lock(string $scene, int $expire = 10, bool $autoUnlock = false, int $retry = 5, int|float $sleep = 0.2) 获取竞争锁
+ * @method void close() 关闭连接句柄（如果不手动调用则会在实例销毁时自动调用）
+ * @method File connect() 获取连接句柄
+ * @method int|false sAddArray(string $key, array|string $values) 往数组集合中追加值
+ * @method array|false getArray(string $key) 获取数组集合
+ * @method int|false sRemoveArray(string $key, array|string $values) 删除数组集合中的值
+ * @method CacheDriverInterface setSerialize(Closure|string $set = 'serialize', Closure|string $get = 'unserialize') 设置序列化方法
+ * @method string getTagKey(string $tag) 获取标签key
+ * @method CacheTagInterface tag(array|string $tag) 标签
+ * @method array|false getTags() 获取所有缓存标签
+ * @method string getTagStoreName() 获取标签仓库名称
+ * @method string getCacheKey(string $key) 获取实际的缓存标识
  */
 class CacheManager
 {
