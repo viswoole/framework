2e199d29644cf7ef3e5a872893c21d90c2058d40
ZhuChongLin <8210856@qq.com>
2024-09-10 22:47:44 +0800

    默认监听器示例

--- config/listens.php
+++ config/listens.php
@@ -1,20 +1,12 @@
 <?php
 // +----------------------------------------------------------------------
-// | 全局事件监听注册
-// | return['event'=>listen::class|'event'=>callback]
+// | 事件监听注册
 // +----------------------------------------------------------------------
 
 declare (strict_types=1);
 
-return [
-  // 应用开始初始化
-  'AppInit' => [],
-  // 应用初始化完成
-  'AppInitialized' => [],
-  // 应用销毁即将销毁
-  'AppDestroy' => [],
-  // SWOOLE服务创建完成
-  'ServerCreate' => [],
-  // SWOOLE服务启动
-  'ServerStart' => []
-];
+use Viswoole\Core\Facade\Event;
+
+Event::on('AppInitialized', function () {
+  echo_log('应用初始化完成');
+}, 1);
