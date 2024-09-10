8ce5b32729d5fe74e217320deadfa858b296639d
ZhuChongLin <8210856@qq.com>
2024-09-10 20:02:43 +0800

    调整系统事件

--- config/listens.php
+++ config/listens.php
@@ -7,11 +7,13 @@
 declare (strict_types=1);
 
 return [
-  // 应用初始化完成
+  // 应用开始初始化
   'AppInit' => [],
+  // 应用初始化完成
+  'AppInitialized' => [],
   // 应用销毁即将销毁
   'AppDestroy' => [],
-  // SWOOLE服务创建
+  // SWOOLE服务创建完成
   'ServerCreate' => [],
   // SWOOLE服务启动
   'ServerStart' => []
