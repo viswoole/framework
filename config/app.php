b64bbed89bd501464ae4f68559d4de662ef108f9
ZhuChongLin <8210856@qq.com>
2024-09-10 19:39:28 +0800

    增加TaskService

--- config/app.php
+++ config/app.php
@@ -5,13 +5,18 @@
 
 declare (strict_types=1);
 
+use Viswoole\Core\Service\TaskService;
+
 return [
   // 默认时区
   'default_timezone' => env('default_timezone', 'Asia/Shanghai'),
   // 是否开启调试模式
   'debug' => env('app_debug', true),
   // 服务提供者注册
-  'providers' => [],
+  'providers' => [
+    // swoole异步任务管理服务 如果不使用可以删除
+    TaskService::class
+  ],
   // 命令处理程序注册
   'commands' => []
 ];
