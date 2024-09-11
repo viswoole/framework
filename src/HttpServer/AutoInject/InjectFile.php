a7f2889238749a57f4f2912052ec77200da0d279
ZhuChongLin <8210856@qq.com>
2024-09-11 23:58:11 +0800

    文件验证器构造函数和验证方法中的参数重命名。
    将文件验证器`InjectFile`的构造函数和`validate`方法中的参数名从`$name`更改为`$key`，以更好地与form-data参数名对应。此外，更新了相关错误消息以反映此更改，并在`checkFile`方法中进行了同样的调整，以保持代码的一致性和清晰性。

--- src/HttpServer/AutoInject/InjectFile.php
+++ src/HttpServer/AutoInject/InjectFile.php
@@ -35,14 +35,14 @@ class InjectFile extends RuleAbstract
    * @param string $fileMime 文件媒体类型
    * @param int $maxSize 文件最大长度,单位字节，小于等于0为不限制长度
    * @param int $count 上传文件数量，小于等于0为不限制文件数量
-   * @param string $name form-data参数名，默认对应参数名
+   * @param string $key form-data参数名，默认对应参数名
    * @param string $message
    */
   public function __construct(
     public readonly string $fileMime = '*',
     public readonly int    $maxSize = 0,
     public readonly int    $count = 0,
-    private string         $name = '',
+    private string         $key = '',
     string                 $message = ''
   )
   {
@@ -54,20 +54,20 @@ class InjectFile extends RuleAbstract
    */
   #[Override] public function validate(mixed $value, string $key = ''): mixed
   {
-    $this->name = empty($this->name) ? $key : $this->name;
-    $upload = Request::files($this->name);
+    $this->key = empty($this->key) ? $key : $this->key;
+    $upload = Request::files($this->key);
     if (empty($upload)) {
       if (is_null($value)) return null;
-      $this->error("必须上传 $this->name 文件");
+      $this->error("必须上传 $this->key 文件");
     }
     if ($this->count > 0 && count($upload) !== $this->count) {
-      $this->error("必须上传 $this->count 个 $this->name 文件");
+      $this->error("必须上传 $this->count 个 $this->key 文件");
     }
     foreach ($upload as $item) $this->checkFile($item);
     if (!$value instanceof File) {
       $value = App::factory()->invokeClass(File::class);
     }
-    $value->inject($upload);
+    $value->inject($this->key, $upload);
     return $value;
   }
 
@@ -80,10 +80,10 @@ class InjectFile extends RuleAbstract
   private function checkFile(UploadedFile $file): void
   {
     if ($this->fileMime !== '*' && $file->getClientMediaType() !== $this->fileMime) {
-      $this->error("$this->name 文件的类型必须为 $this->fileMime");
+      $this->error("$this->key 文件的类型必须为 $this->fileMime");
     }
     if ($this->maxSize > 0 && $file->getSize() > $this->maxSize) {
-      $this->error("$this->name 文件大小不能超过 $this->maxSize 字节");
+      $this->error("$this->key 文件大小不能超过 $this->maxSize 字节");
     }
   }
 }
