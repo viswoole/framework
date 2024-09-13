93793b8a586c18fbd93df8b8e195ce85c0f2971e
ZhuChongLin <8210856@qq.com>
2024-09-13 15:20:04 +0800

    文件验证器优化：改进文件上传参数处理
    文件验证器的参数管理得到简化，删除了不再使用的内部'key'属性。
    
    另外，验证过程中的错误信息生成已更新，以使用新的'key'参数值，而不是之前构造函数中设置的内部属性。这确保了错误信息的准确性，并更好地与当前上传参数保持一致。
    
    此更改带来了更灵活的参数处理，并减少了代码冗余，使文件验证器的使用更加直观和高效。

--- src/HttpServer/AutoInject/InjectFile.php
+++ src/HttpServer/AutoInject/InjectFile.php
@@ -35,14 +35,12 @@ class InjectFile extends RuleAbstract
    * @param string $fileMime 文件媒体类型
    * @param int $maxSize 文件最大长度,单位字节，小于等于0为不限制长度
    * @param int $count 上传文件数量，小于等于0为不限制文件数量
-   * @param string $key form-data参数名，默认对应参数名
    * @param string $message
    */
   public function __construct(
     public readonly string $fileMime = '*',
     public readonly int    $maxSize = 0,
     public readonly int    $count = 0,
-    private string         $key = '',
     string                 $message = ''
   )
   {
@@ -54,20 +52,19 @@ class InjectFile extends RuleAbstract
    */
   #[Override] public function validate(mixed $value, string $key = ''): mixed
   {
-    $this->key = empty($this->key) ? $key : $this->key;
-    $upload = Request::files($this->key);
+    $upload = Request::files($key);
     if (empty($upload)) {
       if (is_null($value)) return null;
-      $this->error("必须上传 $this->key 文件");
+      $this->error("必须上传 $key 文件");
     }
     if ($this->count > 0 && count($upload) !== $this->count) {
-      $this->error("必须上传 $this->count 个 $this->key 文件");
+      $this->error("必须上传 $this->count 个 $key 文件");
     }
     foreach ($upload as $item) $this->checkFile($item);
     if (!$value instanceof File) {
       $value = App::factory()->invokeClass(File::class);
     }
-    $value->inject($this->key, $upload);
+    $value->inject($key, $upload);
     return $value;
   }
 
