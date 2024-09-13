01aadb5826816bc077c2dc93cd822743bc955d32
ZhuChongLin <8210856@qq.com>
2024-09-13 22:42:26 +0800

    新添加了预注入接口`PreInjectInterface`，用于自定义参数注入。
    此接口定义了一个`inject`方法，旨在实现对容器内某个参数的自定义注入逻辑。如果需要中断注入过程，可以通过抛出`ValidateException`异常来实现。

---
+++ src/Core/Contract/PreInjectInterface.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Core\Contract;
+
+use Viswoole\Core\Exception\ValidateException;
+
+/**
+ * 前置注入参数注解基类
+ *
+ * 用途：如果要在容器注入参数时对某个参数实现自定义注入，则需要继承该类，并实现inject方法。
+ */
+interface PreInjectInterface
+{
+  /**
+   * 注入参数值
+   *
+   * @param string $name 当前正在注入的参数名称
+   * @param mixed $value 参数值,如果没有传递的参数值，则为null
+   * @return mixed 返回要注入的值
+   * @throws ValidateException 如果希望停止注入，则抛出一个ValidateException异常，当然你也可以抛出其他任何异常来终止注入。
+   */
+  public function inject(string $name, mixed $value): mixed;
+}
