734828e17780f8c08fff09a97b7ab5f7ddf3120d
ZhuChongLin <8210856@qq.com>
2024-09-13 22:38:04 +0800

    新功能：添加Post请求参数注入类
    实现了一个新的AutoInject Post注解类，用于注入POST请求参数。该功能简化了POST请求处理，增强了框架的自动注入机制。

---
+++ src/HttpServer/AutoInject/Post.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\HttpServer\AutoInject;
+
+use Attribute;
+use Override;
+use Viswoole\HttpServer\Facade\Request;
+use Viswoole\Router\ApiDoc\Params\PostParamInterface;
+
+/**
+ * 用于注入POST请求参数
+ */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
+class Post implements PostParamInterface
+{
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function inject(string $name, mixed $value): mixed
+  {
+    return Request::post($name, $value);
+  }
+}
