5a5b16f73ae45d022441abd3bb35c9558462e1a2
ZhuChongLin <8210856@qq.com>
2024-09-13 22:38:59 +0800

    新实现的Header注入类，用以支持请求头的自动注入。
    该类实现了`HeaderParamInterface`接口。此类的主要功能是通过`inject`方法支持从请求头中自动注入值。如果请求头中不存在指定的键，将返回null或提供的默认值。
    
    此更新允许开发者更方便地在请求处理中直接使用请求头数据，提高了代码的可读性和开发效率。

---
+++ src/HttpServer/AutoInject/Header.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\HttpServer\AutoInject;
+
+use Attribute;
+use Override;
+use Viswoole\HttpServer\Facade\Request;
+use Viswoole\Router\ApiDoc\Params\HeaderParamInterface;
+
+/**
+ * 注入请求头
+ *
+ * 多个文件为数组`UploadedFile[]`, 单个文件为`UploadedFile`对象
+ */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
+class Header implements HeaderParamInterface
+{
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function inject(string $name, mixed $value): string|null
+  {
+    return Request::getHeader($name, $value);
+  }
+}
