c960f8cfc1092f4ecab6e7fff909e8700bd42879
ZhuChongLin <8210856@qq.com>
2024-09-13 22:41:54 +0800

    文档：新增四个参数类型的接口定义
    为API文档参数解析添加新接口定义，包括文件参数（FileParamInterface）、GET请求查询参数（GetParamInterface）、请求头参数（HeaderParamInterface）和POST请求参数（PostParamInterface）。这些接口将规范服务如何注入对应的参数类型，以保证文档能正确解析参数信息。

---
+++ src/Router/ApiDoc/Params/FileParamInterface.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Router\ApiDoc\Params;
+
+use Viswoole\Core\Contract\PreInjectInterface;
+
+/**
+ * 参数需带有实现了该接口的注解声明，文档才能解析参数类型为文件参数。
+ *
+ * 使用路由的服务应该实现该接口，并在inject方法中注入对应的文件。
+ */
+interface FileParamInterface extends PreInjectInterface
+{
+
+}
