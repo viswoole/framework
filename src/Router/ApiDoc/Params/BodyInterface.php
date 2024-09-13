fee5d6e63b018cf4cc68336e5fea0750d2667322
ZhuChongLin <8210856@qq.com>
2024-09-13 22:47:30 +0800

    新接口BodyInterface提升文档解析能力
    创建了新的BodyInterface，用作文档解析中请求body参数声明的标记。此接口扩展了PreInjectInterface，增强了框架内API文档生成的可读性和便利性。

---
+++ src/Router/ApiDoc/Params/BodyInterface.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Router\ApiDoc\Params;
+
+use Viswoole\Core\Contract\PreInjectInterface;
+
+/**
+ * 该接口为文档解析请求body参数声明提供了一个标记。
+ */
+interface BodyInterface extends PreInjectInterface
+{
+
+}
