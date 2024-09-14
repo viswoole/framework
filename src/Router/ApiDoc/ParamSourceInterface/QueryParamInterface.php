f45c88d4d7dd259604d5bb8adad27ce104044bc1
ZhuChongLin <8210856@qq.com>
2024-09-15 00:17:33 +0800

    refactor(router): 重命名GetParamInterface为QueryParamInterface
    将GetParamInterface重命名为QueryParamInterface，以更好地反映其在API文档中的作用。此更改有助于清晰地区分参数源接口。

--- src/Router/ApiDoc/ParamSourceInterface/QueryParamInterface.php
+++ src/Router/ApiDoc/ParamSourceInterface/QueryParamInterface.php
@@ -13,14 +13,14 @@
 
 declare (strict_types=1);
 
-namespace Viswoole\Router\ApiDoc\Body;
+namespace Viswoole\Router\ApiDoc\ParamSourceInterface;
 
 /**
  * 参数需带有实现了该接口的注解声明，文档才能解析参数类型为get请求查询参数。
  *
  * 使用路由的服务应该实现该接口，并在inject方法中注入对应的查询参数。
  */
-interface GetParamInterface extends BodyInterface
+interface QueryParamInterface extends BaseSourceInterface
 {
 
 }
