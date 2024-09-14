dbb4b2917387c0e085b728ba57c2bb6aa049426d
ZhuChongLin <8210856@qq.com>
2024-09-15 06:52:46 +0800

    refactor(router): 改进Returned注解以支持数组和字符串数据
    返回值注解现在能够解析索引数组和关联数组。解析逻辑已更新，以在数据是数组时生成相应的ArrayStructure或ObjectStructure。此外，支持传入字符串类型的响应数据。

--- src/Router/Annotation/Returned.php
+++ src/Router/Annotation/Returned.php
@@ -16,13 +16,18 @@ declare (strict_types=1);
 namespace Viswoole\Router\Annotation;
 
 use Attribute;
+use Closure;
 use JsonSerializable;
 use Override;
+use UnitEnum;
+use Viswoole\Core\Common\Arr;
+use Viswoole\Router\ApiDoc\Structure\ArrayTypeStructure;
+use Viswoole\Router\ApiDoc\Structure\BaseTypeStructure;
+use Viswoole\Router\ApiDoc\Structure\FieldStructure;
+use Viswoole\Router\ApiDoc\Structure\ObjectStructure;
 
 /**
  * 返回值注解
- *
- * 遵循 OpenAPI 规范
  */
 #[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_FUNCTION | Attribute::IS_REPEATABLE)]
 class Returned implements JsonSerializable
@@ -37,21 +42,137 @@ class Returned implements JsonSerializable
   const string TYPE_TEXT = 'text/plain';
   /** 二进制流 */
   const string TYPE_STREAM = 'application/octet-stream';
+  /**
+   * @var array|string 示例响应数据
+   */
+  public array|string $data;
+  /**
+   * @var BaseTypeStructure data结构
+   */
+  public BaseTypeStructure $structure;
 
   /**
    * @param string $title 标题
-   * @param mixed $data 响应数据
+   * @param array|string $data 支持传入数组或者字符串
    * @param int $statusCode 状态码，默认为200
    * @param string $type 响应类型，默认为application/json
    */
   public function __construct(
     public string $title,
-    public mixed  $data,
+    array|string  $data,
     public int    $statusCode = 200,
     public string $type = self::TYPE_JSON
   )
   {
+    if (is_array($data)) {
+      if (Arr::isIndexArray($data)) {
+        // 解析索引数组
+        [$data, $arrayType] = $this->parseValueType($data);
+        $this->structure = $arrayType;
+        $this->data = $data;
+      } else {
+        [$data, $props] = $this->parseArrayObject($data);
+        $this->data = $data;
+        $this->structure = new ObjectStructure($props);
+      }
+    } else {
+      $this->data = $data;
+      $this->structure = new BaseTypeStructure('string');
+    }
+  }
 
+  /**
+   * 解析值类型
+   *
+   * @param mixed $value
+   * @return array{0:mixed,1:BaseTypeStructure}
+   */
+  private function parseValueType(mixed $value): array
+  {
+    $builtin = [
+      'boolean' => 'bool',
+      'integer' => 'int',
+      'double' => 'float',
+      'string' => 'string',
+      'NULL' => 'null'
+    ];
+    $type = gettype($value);
+    // 内置类型
+    if (array_key_exists($type, $builtin)) {
+      return [$value, new BaseTypeStructure($builtin[$type])];
+    }
+    switch ($type) {
+      case 'array':
+        if (empty($value)) {
+          return [$value, new BaseTypeStructure('array')];
+        }
+        if (Arr::isIndexArray($value)) {
+          $items = [];
+          foreach ($value as $index => $item) {
+            [$item, $type] = $this->parseValueType($item);
+            $value[$index] = $item;
+            $items[] = $type;
+          }
+          $arrayType = new ArrayTypeStructure(...$items);
+          return [$value, $arrayType];
+        } else {
+          [$data, $props] = $this->parseArrayObject($value);
+          $value = $data;
+          // 关联数组，视为对象
+          return [$value, new ObjectStructure($props)];
+        }
+      case 'object':
+        if ($value instanceof Closure) {
+          return ['Closure', new BaseTypeStructure('mixed')];
+        }
+        if ($value instanceof UnitEnum) {
+          return [$value->name, new BaseTypeStructure('string')];
+        }
+        return [$value, new ObjectStructure($value, true)];
+      default:
+        // 未知类型 或 资源类型
+        return ['unknown', new BaseTypeStructure('mixed')];
+    }
+  }
+
+  /**
+   * 解析数组对象结构
+   *
+   * @param array $data
+   * @return array{0:array,1:FieldStructure[]}
+   */
+  private function parseArrayObject(array $data): array
+  {
+    $props = [];
+    $showData = [];
+    foreach ($data as $key => $value) {
+      [$key, $description, $allowNull] = $this->parseKey((string)$key);
+      $showData[$key] = $value;
+      [$value, $type] = $this->parseValueType($value);
+      $props[] = new FieldStructure($key, $description, $allowNull, $value, $type);
+    }
+    $data = $showData;
+    return [$data, $props];
+  }
+
+  /**
+   * 解析键名
+   *
+   * @param string $key
+   * @return array
+   */
+  private function parseKey(string $key): array
+  {
+    $array = explode('|', $key);
+    $key = trim($array[0]);
+    if (str_ends_with($key, '?')) {
+      $key = substr($key, 0, -1);
+      $allowNull = true;
+    } else {
+      $allowNull = false;
+    }
+    $description = $array[1] ?? '';
+    return [trim($key), trim($description), $allowNull];
   }
 
   /**
