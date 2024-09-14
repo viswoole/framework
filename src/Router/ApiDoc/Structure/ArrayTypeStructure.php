d0ea58cdedd0c72931c65984c6802e20ca2d075d
ZhuChongLin <8210856@qq.com>
2024-09-15 06:22:52 +0800

    新増ArrayTypeStructure类，用以丰富API文档结构描述
    创建了一个新的类`ArrayTypeStructure`，该类继承自`BaseTypeStructure`，用于描述数组结构。此类支持数组元素的添加，并在构建数组名称时考虑了数组元素的类型。该类的引入使得API文档的结构描述更加丰富和准确。

---
+++ src/Router/ApiDoc/Structure/ArrayTypeStructure.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Router\ApiDoc\Structure;
+
+use Override;
+
+/**
+ * 数组结构描述
+ */
+class ArrayTypeStructure extends BaseTypeStructure
+{
+  /**
+   * @var array<string,BaseTypeStructure> 数组元素结构
+   */
+  public array $items = [];
+
+  /**
+   * 构建数组结构
+   *
+   * @param BaseTypeStructure ...$items 数组元素结构
+   */
+  public function __construct(BaseTypeStructure ...$items)
+  {
+    parent::__construct('array');
+    $this->addItem(...$items);
+  }
+
+  /**
+   * 追加数组元素结构
+   *
+   * @access public
+   * @param BaseTypeStructure ...$items
+   * @return void
+   */
+  public function addItem(BaseTypeStructure ...$items): void
+  {
+    foreach ($items as $item) {
+      $this->items[$item->getName()] = $item;
+    }
+    $this->buildName();
+  }
+
+  /**
+   * 打包名称
+   *
+   * @return void
+   */
+  private function buildName(): void
+  {
+    $names = array_keys($this->items);
+    $types = implode(' | ', $names);
+    $this->name = "Array<$types>";
+  }
+
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function jsonSerialize(): array
+  {
+    return [
+      'type' => $this->type,
+      'items' => $this->items
+    ];
+  }
+}
