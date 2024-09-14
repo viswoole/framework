63afd2dfbc57d859c138b9f835dfe7e763a857da
ZhuChongLin <8210856@qq.com>
2024-09-15 01:26:44 +0800

    路由注释中描述属性的命名已从`describe`改为`description`，以提供更清晰和更具描述性的属性名称。此更改仅影响属性名称，不影响功能。

--- src/Router/Annotation/AnnotationRouteAbstract.php
+++ src/Router/Annotation/AnnotationRouteAbstract.php
@@ -24,20 +24,20 @@ abstract class AnnotationRouteAbstract
   /**
    * @param string|string[]|null $paths null则为当前方法名
    * @param string|null $title 路由标题，null则会自动获取注释中的标题
-   * @param string $describe 路由描述，null则会自动获取注释
+   * @param string $description 路由描述，null则会自动获取注释
    * @param string|array|null $methods 请求方法
    * @param array{middleware:array,suffix:string[],domain:string[],pattern:array<string,string>,hidden:bool,sort:int,meta:array<string,string>} $options
    */
   public function __construct(
     public string|array|null $paths = null,
     public ?string           $title = null,
-    public string            $describe = '',
+    public string            $description = '',
     public string|array|null $methods = null,
     public array             $options = []
   )
   {
     if (!empty($this->methods)) $this->options['method'] = $this->methods;
-    $this->options['describe'] = $this->describe;
+    $this->options['description'] = $this->description;
     if (!empty($this->title)) $this->options['title'] = $this->title;
   }
 }
