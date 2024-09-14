fb4ec9781bf5f19f951c8564d6989dc00ca6bed5
ZhuChongLin <8210856@qq.com>
2024-09-15 00:17:19 +0800

    refactor(framwork): 重新组织ApiDoc参数源接口
    将HeaderParamInterface从Viswoole\Router\ApiDoc\Body移至Viswoole\Router\ApiDoc\ParamSourceInterface，以更好地整理代码结构。
    此更改使参数源接口更统一，更易于维护。

--- src/Router/ApiDoc/ParamSourceInterface/HeaderParamInterface.php
+++ src/Router/ApiDoc/ParamSourceInterface/HeaderParamInterface.php
@@ -13,14 +13,14 @@
 
 declare (strict_types=1);
 
-namespace Viswoole\Router\ApiDoc\Body;
+namespace Viswoole\Router\ApiDoc\ParamSourceInterface;
 
 /**
  * 参数需带有实现了该接口的注解声明，文档才能解析参数类型为请求头。
  *
  * 使用路由的服务应该实现该接口，并在inject方法中注入对应的请求头。
  */
-interface HeaderParamInterface extends BodyInterface
+interface HeaderParamInterface extends BaseSourceInterface
 {
 
 }
