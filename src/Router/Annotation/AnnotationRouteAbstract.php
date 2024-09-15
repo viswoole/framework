654531836cc572d97fdac1d8480740f621c2ebb0
ZhuChongLin <8210856@qq.com>
2024-09-16 01:05:21 +0800

    路由注解类的参数和选项进行了重组和类型定义，以提高代码的清晰度和可维护性。
    具体来说，请求方式现在通过`$method`参数指定，中间件通过`$middleware`数组参数定义。同时，路由描述、标题和其他路由选项如域名、模式等则整合到`$options`数组中。这些改动旨在使路由配置更加结构化，便于开发者理解和操作。

--- src/Router/Annotation/AnnotationRouteAbstract.php
+++ src/Router/Annotation/AnnotationRouteAbstract.php
@@ -23,21 +23,21 @@ abstract class AnnotationRouteAbstract
 {
   /**
    * @param string|string[]|null $paths null则为当前方法名
+   * @param string|string[]|null $method 请求方式
+   * @param array $middleware 中间件
    * @param string|null $title 路由标题，null则会自动获取注释中的标题
-   * @param string $description 路由描述，null则会自动获取注释
-   * @param string|array|null $methods 请求方法
-   * @param array{middleware:array,suffix:string[],domain:string[],pattern:array<string,string>,hidden:bool,sort:int,meta:array<string,string>} $options
+   * @param array{description:string,suffix:string[],domain:string[],pattern:array<string,string>,hidden:bool,sort:int,meta:array<string,string>} $options
    */
   public function __construct(
     public string|array|null $paths = null,
+    public string|array|null $method = null,
+    array                    $middleware = [],
     public ?string           $title = null,
-    public string            $description = '',
-    public string|array|null $methods = null,
     public array             $options = []
   )
   {
-    if (!empty($this->methods)) $this->options['method'] = $this->methods;
-    $this->options['description'] = $this->description;
-    if (!empty($this->title)) $this->options['title'] = $this->title;
+    if (!array_key_exists('middleware', $this->options)) {
+      $this->options['middleware'] = $middleware;
+    }
   }
 }
--- src/Router/Annotation/RouteController.php
+++ src/Router/Annotation/RouteController.php
@@ -24,21 +24,21 @@ class RouteController extends AnnotationRouteAbstract
 {
   /**
    * @param array|string|null $prefix 前缀，null代表当前控制器类名称
+   * @param string|array|null $method 请求方式
+   * @param array $middleware 中间件
    * @param string|null $title 标题, 默认自动获取注释中的标题
-   * @param string $description 描述
-   * @param array|string|null $methods 请求方法
    * @param string|null $server 服务器名称
-   * @param array{middleware:array,suffix:string[],domain:string[],pattern:array<string,string>,hidden:bool,sort:int,meta:array<string,string>} $options 更多配置选项
+   * @param array{description:string,suffix:string[],domain:string[],pattern:array<string,string>,hidden:bool,sort:int,meta:array<string,string>} $options 更多配置选项
    */
   public function __construct(
-    array|string|null $prefix = null,
-    ?string           $title = null,
-    string            $description = '',
-    array|string      $methods = null,
-    public ?string    $server = null,
-    array             $options = []
+    array|string|null        $prefix = null,
+    public string|array|null $method = null,
+    array                    $middleware = [],
+    public ?string           $title = null,
+    public ?string           $server = null,
+    array                    $options = []
   )
   {
-    parent::__construct($prefix, $title, $description, $methods, $options);
+    parent::__construct($prefix, $method, $middleware, $title, $options);
   }
 }
