597f9c6435cb03755e13cb3ebd8774bfd4852f4f
ZhuChongLin <8210856@qq.com>
2024-09-16 00:25:24 +0800

    中间件类API文档更新
    更新了中间件管理器的API文档注释，以反映处理中间件注册和中间件过程的新方法。此更改确保文档准确地描述了当前功能和使用方式。
    
    更改包括：
    - `register` 方法现在接受 `callable`、`string` 或 `array` 类型的处理器。- `process` 方法现在将处理器参数简单地标记为 `callable` 类型。

--- src/Core/Facade/Middleware.php
+++ src/Core/Facade/Middleware.php
@@ -15,15 +15,14 @@ declare (strict_types=1);
 
 namespace Viswoole\Core\Facade;
 
-use Closure;
 use Override;
 use Viswoole\Core\Facade;
 
 /**
  * 中间件管理器
  *
- * @method static void register(Closure|string $handler, string $server = null) 注册中间件
- * @method static mixed process(callable|array|string $handler, array $middlewares = []) 运行中间件
+ * @method static void register(callable|string|array $handler, string $server = null) 注册中间件
+ * @method static mixed process(callable $handler, array $middlewares = []) 运行中间件
  */
 class Middleware extends Facade
 {
