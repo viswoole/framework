902ba853daf1812e1fbf2fcd34cb1a6038aeb3ae
ZhuChongLin <8210856@qq.com>
2024-09-19 02:32:37 +0800

    路由组和控制器注解重构
    将RouteController注解的继承从AnnotationRouteAbstract更改为RouteAnnotation，以统一控制器注解与其他路由注解的行为。此更改简化了注解结构，并为将来的功能扩展提供了更清晰的路径。

--- src/Router/Annotation/RouteController.php
+++ src/Router/Annotation/RouteController.php
@@ -17,28 +17,10 @@ namespace Viswoole\Router\Annotation;
 use Attribute;
 
 /**
- * Controller注解
+ * 路由分组，Controller注解
  */
 #[Attribute(Attribute::TARGET_CLASS)]
-class RouteController extends AnnotationRouteAbstract
+class RouteController extends RouteAnnotation
 {
-  /**
-   * @param array|string|null $prefix 前缀，null代表当前控制器类名称
-   * @param string|array|null $method 请求方式
-   * @param array $middleware 中间件
-   * @param string|null $title 标题, 默认自动获取注释中的标题
-   * @param string|null $server 服务器名称
-   * @param array{description:string,suffix:string[],domain:string[],pattern:array<string,string>,hidden:bool,sort:int,meta:array<string,string>} $options 更多配置选项
-   */
-  public function __construct(
-    array|string|null        $prefix = null,
-    public string|array|null $method = null,
-    array                    $middleware = [],
-    public ?string           $title = null,
-    public ?string           $server = null,
-    array                    $options = []
-  )
-  {
-    parent::__construct($prefix, $method, $middleware, $title, $options);
-  }
+  protected string $type = 'group';
 }
