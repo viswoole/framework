3bbe84126163ab08005fbb2fafe2c91b1fd2f1ad
ZhuChongLin <8210856@qq.com>
2024-09-18 23:57:28 +0800

    新功能：实现路由分组功能以简化路由管理
    引入了RouteGroup类，扩展了路由功能，支持路由分组。这将帮助在Viswoole框架中更有效地管理和组织路由，特别是对于具有多个子路由的情况。主要特性包括：- 实现getItem方法以获取分组内的子路由。
    - 添加addItem方法允许向路由分组中添加路由项。
    - 扩展setParentId方法以设置分组内所有子路由的父ID。
    - 确保通过verifyHandler方法验证路由处理程序，即使handler为空。
    
    此功能改进了路由处理的灵活性和效率，特别适用于需要按功能或模块组织路由的应用程序。

---
+++ src/Router/Route/RouteGroup.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace Viswoole\Router\Route;
+
+use Closure;
+
+/**
+ * 路由组
+ */
+class RouteGroup extends BaseRoute
+{
+  protected string $type = 'group';
+  /**
+   * @var RouteItem[]|static[] 分组、域名路由存储items
+   */
+  private array $children = [];
+
+  /**
+   * 获取子路由
+   *
+   * @param string|null $id 路由id,不传时返回所有子路由
+   * @return array|RouteItem|RouteGroup|null
+   */
+  public function getItem(string $id = null): null|array|RouteItem|RouteGroup
+  {
+    if ($this->handler instanceof Closure) {
+      call_user_func($this->handler);
+      $this->handler = null;
+      uasort($this->children, function (BaseRoute $a, BaseRoute $b) {
+        return $b->getSort() <=> $a->getSort();
+      });
+    }
+    if (empty($id)) {
+      return $this->children;
+    } else {
+      return $this->children[$id] ?? null;
+    }
+  }
+
+  /**
+   * 往路由分组中追加一个路由
+   *
+   * @param BaseRoute $item
+   * @return void
+   */
+  public function addItem(BaseRoute $item): void
+  {
+    $this->children[$item->getId()] = $item;
+  }
+
+  /**
+   * 设置父级路由ID
+   *
+   * @param ?string $parentId
+   * @return $this
+   */
+  public function setParentId(?string $parentId): static
+  {
+    parent::setParentId($parentId);
+    foreach ($this->children as $item) {
+      $item->setParentId($this->getCiteLink());
+    }
+    return $this;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function verifyHandler(callable|array|string $handler): callable|array
+  {
+    if (empty($handler)) return [];
+    return parent::verifyHandler($handler);
+  }
+}
