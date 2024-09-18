5ded0ff67dd0d71bf64d597249df0c385dc847a5
ZhuChongLin <8210856@qq.com>
2024-09-19 02:35:49 +0800

    路由模块重构：优化注解路由加载机制并修复相关文档注释工具问题。
    - 优化注解路由的加载流程，改进缓存处理逻辑。
    - 重构 `parseController` 方法以简化控制器解析逻辑。
    - 修正 `parseMethod` 方法，增强方法解析的准确性。- 更新缺失的类型注释，提高代码的清晰度和可维护性。
    - 修复 `DocCommentTool` 相关问题，确保文档注释的正确提取和处理。

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -26,6 +26,7 @@ use Viswoole\Core\Middleware;
 use Viswoole\Router\Annotation\AutoRouteController;
 use Viswoole\Router\Annotation\RouteController;
 use Viswoole\Router\Annotation\RouteMapping;
+use Viswoole\Router\ApiDoc\DocCommentTool;
 use Viswoole\Router\Exception\RouteNotFoundException;
 use Viswoole\Router\Route\BaseRoute;
 use Viswoole\Router\Route\Collector;
@@ -66,7 +67,7 @@ class Router extends Collector
     $this->event->emit('RouteInit');
     $this->cache = $config->get('router.cache.enable', false);
     $this->loadConfigRoute();
-//    $this->loadAnnotationRoute();
+    $this->loadAnnotationRoute();
     $this->parseRoute();
     $this->event->emit('RouteLoaded');
   }
@@ -81,6 +82,205 @@ class Router extends Collector
     $loadPaths = $this->config->get('router.route_config_files', []);
     foreach ($loadPaths as $file) {
       require_once $file;
+    }
+  }
+
+  /**
+   * 加载注解路由
+   *
+   * @return void
+   */
+  private function loadAnnotationRoute(): void
+  {
+    $rootPath = getRootPath() . DIRECTORY_SEPARATOR;
+    $directory = $rootPath . 'app/Controller';
+    // 列出指定路径中的文件和目录
+    $controllers = $this->getAllPhpFiles($directory);
+    foreach ($controllers as $controller) {
+      [$fullClass] = $this->getNamespace($controller, $rootPath);
+      $hash = null;
+      // 获取路由缓存
+      if ($this->cache) {
+        // 类文件哈希值
+        $hash = hash_file('md5', $controller);
+        $cacheRouteInfo = RouteTool::getCache(SERVER_NAME . $fullClass, $hash);
+        if (is_array($cacheRouteInfo)) {
+          $this->recordRouteItem($cacheRouteInfo['route']);
+          continue;
+        }
+      }
+      // 没有缓存，则解析路由
+      $routeInfo = $this->parseController($controller, $rootPath);
+      // 如果没有解析到路由则跳过
+      if (empty($routeInfo)) continue;
+      // 记录路由
+      $this->recordRouteItem($routeInfo['route']);
+      // 如果hash不为null则缓存路由
+      if (!$hash) continue;
+      RouteTool::setCache(
+        SERVER_NAME . $fullClass, $hash, $routeInfo['server'], $routeInfo['route']
+      );
+    }
+  }
+
+  /**
+   * 获取所有php文件
+   *
+   * @param string $dir
+   * @return array
+   */
+  private function getAllPhpFiles(string $dir): array
+  {
+    $phpFiles = [];
+    // 打开目录
+    if ($handle = opendir($dir)) {
+      $dir = rtrim($dir, DIRECTORY_SEPARATOR);
+      // 逐个检查目录中的条目
+      while (false !== ($entry = readdir($handle))) {
+        if ($entry != '.' && $entry != '..') {
+          $path = $dir . '/' . $entry;
+
+          // 如果是目录，递归调用该函数
+          if (is_dir($path)) {
+            $phpFiles = array_merge($phpFiles, $this->getAllPhpFiles($path));
+          } elseif (pathinfo($path, PATHINFO_EXTENSION) == 'php') {
+            // 如果是.php文件，添加到结果数组中
+            $phpFiles[] = $path;
+          }
+        }
+      }
+
+      // 关闭目录句柄
+      closedir($handle);
+    }
+
+    return $phpFiles;
+  }
+
+  /**
+   * 获取控制器完全限定名称
+   *
+   * @param string $controller
+   * @param string $rootPath
+   * @return array{0:string,1:string} [0=>完全限定名称,1=>类名称]
+   */
+  private function getNamespace(string $controller, string $rootPath): array
+  {
+    // 获得类名称
+    $className = basename($controller, '.php');
+    // 获得命名空间
+    $classNamespace = str_replace($rootPath, '', $controller);
+    $classNamespace = preg_replace('#^app/#', 'App/', dirname($classNamespace));
+    $classNamespace = str_replace('/', '\\', $classNamespace);
+    // 类完全限定名称Class::class
+    return [$classNamespace . '\\' . $className, $className];
+  }
+
+  /**
+   * 解析控制器
+   *
+   * @param string $file 控制器文件
+   * @param string $rootPath 根目录
+   * @return array{server:string|null,route:RouteGroup}|null
+   */
+  private function parseController(string $file, string $rootPath): ?array
+  {
+    [$fullClass] = $this->getNamespace($file, $rootPath);
+    if (class_exists($fullClass)) {
+      $refClass = new ReflectionClass($fullClass);
+      $className = $refClass->getShortName();
+    } else {
+      return null;
+    }
+    // 获取路由注解属性
+    $classAttributes = $refClass->getAttributes(
+      RouteController::class, ReflectionAttribute::IS_INSTANCEOF
+    );
+    // 没有路由控制器注解属性则不解析
+    if (empty($classAttributes)) return null;
+    /** @var RouteController|AutoRouteController $controller 控制器路由注解实例 */
+    $controller = $classAttributes[0]->newInstance();
+    // 服务名称
+    $serverName = $controller->server ?? SERVER_NAME;
+    // 判断服务名称是否匹配当前服务
+    if (strtolower($serverName) !== strtolower(SERVER_NAME)) return null;
+    // 判断是否设置了描述
+    if (!isset($controller->title)) {
+      $controller->title = DocCommentTool::extractDocTitle($refClass->getDocComment() ?: '');
+    }
+    /** 是否为自动路由 */
+    $isAutoRoute = $controller instanceof AutoRouteController;
+    // 如果类路由注解的paths设置为null则默认为类名称
+    if ($controller->prefix === null) $controller->prefix = $className;
+    // 类完全名称md5值作为路由分组名称
+    if (!$controller->id) $controller->id = RouteTool::generateHashId($fullClass);
+    /**
+     * @var RouteGroup $group 路由分组实例
+     */
+    $group = $controller->create([]);
+    // 类的全部方法
+    $methods = $refClass->getMethods();
+    if (!empty($methods)) $this->parseMethod($methods, $isAutoRoute, $group);
+    return ['server' => $serverName, 'route' => $group];
+  }
+
+  /**
+   * 解析方法
+   *
+   * @param ReflectionMethod[] $methods 方法列表
+   * @param bool $isAutoRoute 是否自动路由
+   * @param RouteGroup $group
+   * @return void
+   */
+  private function parseMethod(array $methods, bool $isAutoRoute, RouteGroup $group): void
+  {
+    if (empty($methods)) return;
+    $class = $methods[0]->getDeclaringClass()->getName();
+    foreach ($methods as $method) {
+      // 判断是否需要创建路由
+      $isCreate = $method->isPublic()
+        && !$method->isConstructor()
+        && !$method->isAbstract()
+        && !$method->isDestructor();
+      // 不需要创建路由则跳过
+      if (!$isCreate) continue;
+      $methodName = $method->getName();
+      // 路由id
+      $methodId = RouteTool::generateHashId($class . '::' . $methodName);
+      // 获取方法注解
+      $methodAttributes = $method->getAttributes(RouteMapping::class);
+      // 方法文档注释
+      $methodDocComment = $method->getDocComment() ?: '';
+      // 构建处理方法
+      $handler = $method->isStatic()
+        ? $class . '::' . $method->getName()
+        : [$class, $method->getName()];
+      // 如果没有设置路由注解，且该类为自动路由则创建路由
+      if (empty($methodAttributes)) {  // 自动路由
+        if (!$isAutoRoute) continue;
+        // 创建新的路由项
+        $routeItem = new RouteItem($method->getName(), $handler, $group, id: $methodId);
+        // 设置标题
+        $routeItem->setTitle(DocCommentTool::extractDocTitle($methodDocComment));
+      } else {
+        // 处理设置了路由注解的方法
+        /** @var RouteMapping $methodAnnotationRoute 注解路由 */
+        $methodAnnotationRoute = $methodAttributes[0]->newInstance();
+        // 设置描述
+        if (!isset($methodAnnotationRoute->title)) {
+          $methodAnnotationRoute->title = DocCommentTool::extractDocTitle($methodDocComment);
+        }
+        // 如果没有设置路由路径则默认为方法名称
+        if (empty($methodAnnotationRoute->prefix)) {
+          $methodAnnotationRoute->prefix = $methodName;
+        }
+        // 设置路由id
+        if (!$methodAnnotationRoute->id) $methodAnnotationRoute->id = $methodId;
+        // 创建路由项
+        $routeItem = $methodAnnotationRoute->create($handler, $group);
+      }
+      // 添加到组的子路由中
+      $group->addItem($routeItem);
     }
   }
 
@@ -175,7 +375,7 @@ class Router extends Collector
       });
       $regex = $this->convertRegex(
         $urlSegments,
-        $this->getRoute($routeIndex)['pattern']
+        $this->getRoute($routeIndex)->getPatterns()
       );
       $this->addDynamicRoute($urlSegments, $regex, $routeIndex);
     } else {
@@ -309,7 +509,7 @@ class Router extends Collector
           // 拿到路由
           $route = $this->getRoute($routes[$regex]);
           // 去除匹配到的key
-          $keys = array_slice(array_keys($route['pattern']), 0, count($matches));
+          $keys = array_slice(array_keys($route->getPatterns()), 0, count($matches));
           // 组合为关联数组
           $pattern = array_combine($keys, $matches);
           break;
@@ -319,19 +519,19 @@ class Router extends Collector
     try {
       if (is_null($route)) throw new RouteNotFoundException('路由未定义');
       // 判断请求方法
-      $this->checkOption($route, 'method', $method);
+      $this->checkOption($route->getMethod(), strtoupper($method));
       // 判断域名
-      $this->checkOption($route, 'domain', $domain);
+      $this->checkOption($route->getDomain(), $domain);
       // 判断伪静态后缀
-      $this->checkOption($route, 'suffix', $ext);
+      $this->checkOption($route->getSuffix(), $ext);
       // 合并参数
       if (!empty($pattern)) {
         if ($callback) $callback($pattern);
         if ($params) $params = array_merge($params, $pattern);
       }
       return $this->middleware->process(function () use ($route, $params) {
-        return invoke($route['handler'], $params ?? []);
-      }, $route['middleware']);
+        return invoke($route->getHandler(), $params ?? []);
+      }, $route->getMiddlewares());
     } catch (RouteNotFoundException $e) {
       // 匹配miss路由
       if (isset($this->missRoutes[$method])) {
@@ -349,222 +549,19 @@ class Router extends Collector
   /**
    * 验证选项
    *
-   * @param RouteItem $route
-   * @param string $option_name
+   * @param array $option
    * @param string $value
    */
   private function checkOption(
-    RouteItem $route,
-    string    $option_name,
-    string    $value
+    array  $option,
+    string $value
   ): void
   {
     if (
-      !in_array('*', $route[$option_name] ?? [])
-      && !in_array($value, $route[$option_name] ?? [])
+      !in_array('*', $option)
+      && !in_array($value, $option)
     ) {
       throw new RouteNotFoundException('路由匹配失败');
     }
-  }
-
-  /**
-   * 加载注解路由
-   *
-   * @return void
-   */
-  private function loadAnnotationRoute(): void
-  {
-    $rootPath = getRootPath() . DIRECTORY_SEPARATOR;
-    $directory = $rootPath . 'app/Controller';
-    // 列出指定路径中的文件和目录
-    $controllers = $this->getAllPhpFiles($directory);
-    foreach ($controllers as $controller) {
-      [$fullClass] = $this->getNamespace($controller, $rootPath);
-      $hash = null;
-      // 获取路由缓存
-      if ($this->cache) {
-        // 类文件哈希值
-        $hash = hash_file('md5', $controller);
-        $cacheRouteInfo = RouteTool::getCache(SERVER_NAME . $fullClass, $hash);
-        if (is_array($cacheRouteInfo)) {
-          $this->recordRouteItem($cacheRouteInfo['route']);
-          continue;
-        }
-      }
-      // 没有缓存，则解析路由
-      $routeInfo = $this->parseController($controller, $rootPath);
-      // 如果没有解析到路由则跳过
-      if (empty($routeInfo)) continue;
-      // 记录路由
-      $this->recordRouteItem($routeInfo['route']);
-      // 如果hash不为null则缓存路由
-      if (!$hash) continue;
-      RouteTool::setCache(
-        SERVER_NAME . $fullClass, $hash, $routeInfo['server'], $routeInfo['route']
-      );
-    }
-  }
-
-  /**
-   * 获取所有php文件
-   *
-   * @param string $dir
-   * @return array
-   */
-  private function getAllPhpFiles(string $dir): array
-  {
-    $phpFiles = [];
-    // 打开目录
-    if ($handle = opendir($dir)) {
-      $dir = rtrim($dir, DIRECTORY_SEPARATOR);
-      // 逐个检查目录中的条目
-      while (false !== ($entry = readdir($handle))) {
-        if ($entry != '.' && $entry != '..') {
-          $path = $dir . '/' . $entry;
-
-          // 如果是目录，递归调用该函数
-          if (is_dir($path)) {
-            $phpFiles = array_merge($phpFiles, $this->getAllPhpFiles($path));
-          } elseif (pathinfo($path, PATHINFO_EXTENSION) == 'php') {
-            // 如果是.php文件，添加到结果数组中
-            $phpFiles[] = $path;
-          }
-        }
-      }
-
-      // 关闭目录句柄
-      closedir($handle);
-    }
-
-    return $phpFiles;
-  }
-
-  /**
-   * 获取控制器完全限定名称
-   *
-   * @param string $controller
-   * @param string $rootPath
-   * @return array{0:string,1:string} [0=>完全限定名称,1=>类名称]
-   */
-  private function getNamespace(string $controller, string $rootPath): array
-  {
-    // 获得类名称
-    $className = basename($controller, '.php');
-    // 获得命名空间
-    $classNamespace = str_replace($rootPath, '', $controller);
-    $classNamespace = preg_replace('#^app/#', 'App/', dirname($classNamespace));
-    $classNamespace = str_replace('/', '\\', $classNamespace);
-    // 类完全限定名称Class::class
-    return [$classNamespace . '\\' . $className, $className];
-  }
-
-  /**
-   * 解析控制器
-   *
-   * @param string $file 控制器文件
-   * @param string $rootPath 根目录
-   * @return array{server:string|null,route:RouteGroup}|null
-   */
-  private function parseController(string $file, string $rootPath): ?array
-  {
-    [$fullClass] = $this->getNamespace($file, $rootPath);
-    if (class_exists($fullClass)) {
-      $refClass = new ReflectionClass($fullClass);
-      $className = $refClass->getShortName();
-    } else {
-      return null;
-    }
-    // 获取路由注解属性
-    $classAttributes = $refClass->getAttributes(
-      RouteController::class, ReflectionAttribute::IS_INSTANCEOF
-    );
-    // 没有路由控制器注解属性则不解析
-    if (empty($classAttributes)) return null;
-    // 类完全名称md5值作为路由分组名称
-    $groupId = RouteTool::generateHashId($fullClass);
-    /** @var RouteController|AutoRouteController $controller 控制器路由注解实例 */
-    $controller = $classAttributes[0]->newInstance();
-    // 服务名称
-    $serverName = $controller->server ?? SERVER_NAME;
-    // 判断服务名称是否匹配当前服务
-    if (strtolower($serverName) !== strtolower(SERVER_NAME)) return null;
-    // 判断是否设置了描述
-    if (!isset($controller->title)) {
-      $controller->options['title'] = $this->getDocComment($refClass);
-    }
-    /** 是否为自动路由 */
-    $isAutoRoute = $controller instanceof AutoRouteController;
-    // 如果类路由注解的paths设置为null则默认为类名称
-    if ($controller->paths === null) $controller->paths = $className;
-    /** 分组路由实例 */
-    $group = new RouteGroup($controller->paths, [], id: $groupId);
-    // 类的全部方法
-    $methods = $refClass->getMethods();
-    // 处理类的方法
-    foreach ($methods as $method) {
-      // 判断是否需要创建路由
-      $isCreate = $method->isPublic()
-        && !$method->isConstructor()
-        && !$method->isAbstract()
-        && !$method->isDestructor();
-      // 不需要创建路由则跳过
-      if (!$isCreate) continue;
-      // 方法id
-      $methodId = RouteTool::generateHashId($fullClass . $method->getName());
-      // 获取方法注解
-      $methodAttributes = $method->getAttributes(RouteMapping::class);
-      // 构建处理方法
-      $handler = $method->isStatic() ?
-        $refClass->getName() . '::' . $method->getName()
-        : [$refClass->getName(), $method->getName()];
-      // 如果没有设置路由注解，且该类为自动路由则创建路由
-      if (empty($methodAttributes) && $isAutoRoute) {  // 自动路由
-        // 创建新的路由项
-        $routeItem = new RouteItem(
-          $method->getName(), $handler, $group, id: $methodId
-        );
-        // 设置描述
-        $routeItem->setTitle($this->getDocComment($method));
-        // 添加到组的子路由中
-        $group->addItem($routeItem);
-      } elseif (isset($methodAttributes[0])) {
-        // 处理设置了路由注解的方法
-        /** @var RouteMapping $methodAnnotationRoute 注解路由 */
-        $methodAnnotationRoute = $methodAttributes[0]->newInstance();
-        // 设置描述
-        if (!isset($methodAnnotationRoute->title)) {
-          $methodAnnotationRoute->options['title'] = $this->getDocComment($method);
-        }
-        $path = $methodAnnotationRoute->paths ?: $method->getName();
-        // 创建新的路由项
-        $routeItem = new RouteItem($path, $handler, $group, id: $methodId);
-        // 批量设置选项
-//        $routeItem->options($methodAnnotationRoute->options);
-        // 添加到组的子路由中
-        $group->addItem($routeItem);
-      }
-    }
-    return ['server' => $serverName, 'route' => $group];
-  }
-
-  /**
-   * 获取注释
-   *
-   * @param ReflectionClass|ReflectionMethod $reflector
-   * @return string
-   */
-  private function getDocComment(ReflectionClass|ReflectionMethod $reflector): string
-  {
-    $classDocComment = $reflector->getDocComment();
-    if ($classDocComment) {
-      if (preg_match('/^\s+\*\s+([^@\n][^\n]*)$/m', $classDocComment, $matches)) {
-        $classDocComment = trim($matches[1]);
-      } else {
-        $classDocComment = '';
-      }
-    } else {
-      $classDocComment = '';
-    }
-    return $classDocComment;
   }
 }
