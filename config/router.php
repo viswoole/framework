c2b7e06157062af282eaef79c885f4ed587ff7a3
ZhuChongLin <8210856@qq.com>
2024-09-19 21:18:18 +0800

    路由配置增强：添加路由缓存和API文档配置
    路由缓存功能已禁用并设置缓存目录为'runtime/route'。API文档配置已添加，目前全局返回数据声明、请求头、查询参数和请求参数配置均为空。这些新增项旨在提供更灵活的路由管理和文档生成配置。

--- config/router.php
+++ config/router.php
@@ -16,8 +16,28 @@ return [
   'method' => '*',
   // 默认的路由变量正则表达式
   'default_pattern_regex' => '[\w\.]+',
-  // 要加载的路线配置文件
+  // 要加载的路由定义文件
   'route_config_files' => [
     BASE_PATH . '/config/route/route.php'
-  ]
+  ],
+  // 路由缓存配置
+  'cache' => [
+    // 是否开启路由缓存
+    'enable' => false,
+    // 路由缓存存放目录
+    'path' => BASE_PATH . '/runtime/route'
+  ],
+  // 路由文档配置
+  'api_doc' => [
+    // 是否启用
+    'enable' => false,
+    // 全局返回数据声明
+    'returned' => [],
+    // 全局请求头
+    'header' => [],
+    // 全局查询参数(GET)
+    'query' => [],
+    // 全局请求参数(POST)
+    'body' => [],
+  ],
 ];
