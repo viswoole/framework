2ed0acbc6659bac158af0b028e560831107fdd38
ZhuChongLin <8210856@qq.com>
2024-09-19 20:56:39 +0800

    refactor(route): 更新路由配置，优化默认和404路由定义
    更新了路由配置文件，对默认路由和404路由的注册方式进行优化。默认路由现在使用Facade的Router类，并设置了一个标题。404路由的处理现在通过miss方法定义，确保了对未找到的路由的正确处理。

--- config/route/route.php
+++ config/route/route.php
@@ -7,13 +7,14 @@ declare (strict_types=1);
 
 use Viswoole\HttpServer\Request;
 use Viswoole\HttpServer\Response;
-use Viswoole\Router\Route;
+use Viswoole\Router\Facade\Router;
 
-Route::get('/', function (Request $request, Response $response) {
+// 该路由系统生成的默认路由，访问根目录时，返回一个随机数字
+Router::get('/', function (Request $request, Response $response) {
   return $response->send('<h1>Hello Viswoole. #' . rand(1000, 9999) . '</h1>');
+})->setTitle('Welcome');
+
+// 定义一个404路由
+Router::miss(function (Response $response) {
+  return $response->status(404)->json(['message' => 'Not Found']);
 });
-Route::miss(
-  function (Response $response) {
-    return $response->status(404)->json(['message' => 'Not Found']);
-  }
-);
