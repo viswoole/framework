e4b884424b6daf8e6f2194a6d642c549d925e4fe
ZhuChongLin <8210856@qq.com>
2024-09-21 00:31:41 +0800

    已从`server.php`配置中移除事件监听。
    此更改通过消除不必要的控制台输出，优化了日志记录过程，
    有助于在生产环境中减少干扰和混乱。同时，调整了事件处理函数的映射，以备将来可能的需求。

--- config/server.php
+++ config/server.php
@@ -7,8 +7,6 @@ declare (strict_types=1);
 
 use Swoole\Constant;
 use Swoole\Http\Server as httpServer;
-use Swoole\Server as SwooleServer;
-use Viswoole\Core\Console\Output;
 use Viswoole\HttpServer\Exception\HttpExceptionHandle;
 use Viswoole\HttpServer\HttpEventHandle;
 
@@ -80,14 +78,5 @@ return [
     Constant::OPTION_LOG_LEVEL => SWOOLE_LOG_WARNING
   ],
   // 全局EVENTS
-  'events' => [
-    Constant::EVENT_START => function (SwooleServer $server): void {
-      $serverName = SERVER_NAME;
-      Output::echo("$serverName 服务启动主进程PID:" . $server->master_pid, 'NOTICE', backtrace: 0);
-    },
-    Constant::EVENT_SHUTDOWN => function (): void {
-      $serverName = SERVER_NAME;
-      Output::echo("$serverName 服务已经安全关闭", 'NOTICE', backtrace: 0);
-    }
-  ]
+  'events' => []
 ];
