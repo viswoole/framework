ee0acc230f6ed999ce897c0f41df6404daef4008
ZhuChongLin <8210856@qq.com>
2024-09-21 23:14:57 +0800

    文档更新：清晰表述缓存配置选项
    更新了缓存配置文件中的注释，以明确配置驱动程序时的格式。此更改仅影响文档的清晰度，不涉及功能更改。

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     // 驱动类需继承Viswoole\Cache\Driver，
     // 或实现Viswoole\Cache\Contract\CacheDriverInterface接口。
     // 内置了两种缓存驱动：File、Redis
-    // 数组形式配置 'name'=>['diver'=>diver::class,'options'=>object|array]
+    // 数组形式配置 'name'=>['diver'=>diver::class,'options'=>array] options为驱动类的构造参数列表
     // 字符串形式配置 'name' => diver::class
     // 实例形式配置 'name' => new Redis(env('REDIS_HOST', '127.0.0.1'))
     'file' => Cache::FILE_DRIVER
