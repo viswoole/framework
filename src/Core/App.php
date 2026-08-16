a5e622914e735e8d1c41f7e0a3570f8d2a87921b
ZhuChongLin <8210856@qq.com>
2026-08-16 22:43:02 +0800

    chore(release): 发布v1.0.14版本

--- src/Core/App.php
+++ src/Core/App.php
@@ -51,7 +51,7 @@ class App extends Container
   /**
    * 框架版本号
    */
-  public const string VERSION = '1.0.13';
+  public const string VERSION = '1.0.14';
   /**
    * @var App 当前应用容器实例（单例）
    */
